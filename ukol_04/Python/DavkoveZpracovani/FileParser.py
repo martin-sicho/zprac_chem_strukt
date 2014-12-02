28ebfabfe5333b8bee2b46119dc80c88c4b87472
martin-sicho <sicho.martin@gmail.com>
2014-12-02 22:45:47 +0100

    implementace zbytku 4. ulohy krome 4. ukolu

--- ukol_04/Python/DavkoveZpracovani/FileParser.py
+++ ukol_04/Python/DavkoveZpracovani/FileParser.py
@@ -9,13 +9,17 @@ class FileParser:
     before_function = None
     after_function = None
     parsing_function = None
+    ret_function = None
 
-    def __init__(self, parsing_method, input_dir, before=None, after=None):
+    def __init__(self, parsing_method, input_dir, args=[], before=None, after=None, ret_function=None):
         if before:
             self.before_function = before
         if after:
             self.after_function = after
+        if ret_function:
+            self.ret_function = ret_function
         self.parsing_function = parsing_method
+        self.parsing_function.args = args
         self.input_dir = input_dir
         files = [x for x in os.listdir(input_dir)]
         files.sort()
@@ -37,7 +41,10 @@ class FileParser:
                     if mol:
                         mol.SetProp("_Name", number)
                         self.current_mol = mol
-                        self.parsing_function(mol, path=smilefile_path)
+                        if self.ret_function:
+                            self.ret_function(self.parsing_function(mol, path=smilefile_path))
+                        else:
+                            self.parsing_function(mol, path=smilefile_path)
                     else:
                         sys.stderr.write("Molecule #{0} ({1}) at {2} FAILED to parse.\n".format(number, smiles, smilefile_path))
 
--- ukol_04/Python/DavkoveZpracovani/main.py
+++ ukol_04/Python/DavkoveZpracovani/main.py
@@ -2,6 +2,10 @@ import sys, os
 from rdkit import Chem
 from FileParser import FileParser
 
+from rdkit.Chem import Descriptors
+
+# 1 #
+
 def save_InChi(mol, **kwargs):
     path = kwargs["path"]
     name = mol.GetProp("_Name")
@@ -14,13 +18,86 @@ def delete_old_Inchi_files(*args, **kwargs):
     for old_file in old_files:
         os.remove(old_file)
 
+# 2 #
+
+def save_mols_with_nitrile(mol, **kwargs):
+    weight = Descriptors.ExactMolWt(mol)
+    if weight < 150.0:
+        patt = Chem.MolFromSmarts('[NX1]#[CX2]')
+        if mol.HasSubstructMatch(patt):
+            return mol
+
+def save_mols_with_nitrile_ret_function(mol):
+    if mol:
+        with open("mols_with_nitrile_group.smi", mode="a") as outfile:
+            outfile.write(Chem.MolToSmiles(mol) + "\n")
+
+def delete_old_outfiles(*args, **kwargs):
+    old_files = [ x for x in os.listdir(".") if x.endswith(".smi")]
+    for old_file in old_files:
+        os.remove(old_file)
+
+# 3 #
+
+def getMoleculesMolWeightLessThan(mol, **kwargs):
+    threshold = getMoleculesMolWeightLessThan.args[0]
+    if Descriptors.ExactMolWt(mol) < threshold:
+        return mol
+
+def appendToSet(mol):
+    if mol:
+        appendToSet.set.add(mol)
+appendToSet.set = set()
+
+# 4 # FIXME: do this correctly
+
+# def appendToAcceptedSet(mol, **kwargs):
+#     accepted = appendToAcceptedSet.args[0]
+#     atom_count = mol.GetNumHeavyAtoms()
+#     if atom_count <= 4:
+#         if not accepted:
+#             accepted.add(mol)
+#             return
+#
+#         to_remove = set()
+#         found_superstructure = False
+#         for item in accepted:
+#             if mol.HasSubstructMatch(item):
+#                 to_remove.add(item)
+#             if item.HasSubstructMatch(mol):
+#                 found_superstructure = True
+#
+#         accepted.difference_update(to_remove)
+#         if not found_superstructure:
+#             accepted.add(mol)
+#     else:
+#         to_remove = set()
+#         for item in accepted:
+#             if mol.HasSubstructMatch(item):
+#                 to_remove.add(item)
+#         accepted.difference_update(to_remove)
 
 def main(args):
     INPUT_DIR = "../../smiles/"
 
+    # 1 #
     save_InChi_parser = FileParser(save_InChi, INPUT_DIR, before=delete_old_Inchi_files)
     # save_InChi_parser.run()
 
+    # 2 #
+    save_some_mols_parser = FileParser(save_mols_with_nitrile, INPUT_DIR, ret_function=save_mols_with_nitrile_ret_function, before=delete_old_outfiles)
+    # save_some_mols_parser.run()
+
+    # 3 #
+    return_set = set()
+    filter_method_parser = FileParser(getMoleculesMolWeightLessThan, INPUT_DIR, args=[150], ret_function=appendToSet)
+    appendToSet.set = return_set
+    # filter_method_parser.run()
+
+    # 4 #
+    # accepted_set = set()
+    # filter_method_parser = FileParser(appendToAcceptedSet, INPUT_DIR, args=[accepted_set])
+    # filter_method_parser.run()
 
 if __name__ == "__main__":
     main(sys.argv)
